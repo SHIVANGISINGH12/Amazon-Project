eb122890058fb35a895be6b9dd0d5073bb70243c
Shivangi Singh <shivangiarusingh@gmail.com>
2025-07-14 14:22:33 +0530

    Learn Promises and convert code to promises.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,58 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+// Using PROMISES for asynchronous code.
+//"resolve" lets us control when to go to the next step.
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('sugarBob');
+    });
+  }),
+]).then((values) => {
+    console.log(values);
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+})
+  .then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+*/
+
+/*------Using CALLBACKS for asynchronous code---------------
+
+loadProducts(() => {
+  loadCart(() => {
+    
+  });
+});
+
+*/
